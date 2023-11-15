6e35529b029868bd58d0c0b82cca488c9c15df44
Andrey <andrey-valeriy.burov@mail.ru>
2023-11-15 03:54:36 +0300

    fix round button

--- src/ui/Buttons/RoundButtons/RoundButton/RoundButton.tsx
+++ src/ui/Buttons/RoundButtons/RoundButton/RoundButton.tsx
@@ -3,9 +3,9 @@ import styles from './RoundButton.module.scss'
 import React from 'react'
 import { IRoundButtonProps } from './types'
 
-export default function RoundButton({ icon, color }: IRoundButtonProps) {
+export default function RoundButton({ icon, color, onClick }: IRoundButtonProps) {
 	return (
-		<button className={styles.RoundButton} style={{ backgroundColor: color }}>
+		<button className={styles.RoundButton} style={{ backgroundColor: color }} onClick={onClick}>
 			<div className={styles.IconContainer}>{icon}</div>
 		</button>
 	)
--- src/ui/Buttons/RoundButtons/RoundButton/types.ts
+++ src/ui/Buttons/RoundButtons/RoundButton/types.ts
@@ -3,4 +3,5 @@ import { ReactNode } from 'react'
 export interface IRoundButtonProps {
 	icon: ReactNode
 	color?: string
+	onClick: () => void
 }
