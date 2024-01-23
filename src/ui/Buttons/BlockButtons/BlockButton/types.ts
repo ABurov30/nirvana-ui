2d305759b7f2b7b3672cdb5cae0171f39bde8b95
Andrey Burov <AnBurov@Korusconsulting.ru>
2024-01-23 12:23:01 +0300

    fix

--- src/ui/Buttons/BlockButtons/BlockButton/types.ts
+++ src/ui/Buttons/BlockButtons/BlockButton/types.ts
@@ -3,6 +3,7 @@ import { ButtonHTMLAttributes, ReactNode } from 'react'
 
 export interface IBlockButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	text: string
+	type?: 'submit' | 'reset' | 'button' | undefined
 	icon?: ReactNode
 	onClick?: () => void
 }
