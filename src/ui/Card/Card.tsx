7bc95af0020bbd80f0a6b2d7077192fdad3c411c
Andrey Burov <AnBurov@Korusconsulting.ru>
2024-02-01 00:59:22 +0300

    fix card img widht

--- src/ui/Card/Card.tsx
+++ src/ui/Card/Card.tsx
@@ -7,7 +7,7 @@ import style from './Card.module.scss'
 export default function Card({ srcImg, title, subTitle, onClick }: ICardProps) {
 	return (
 		<div className={style.card} onClick={onClick}>
-			<img alt={title} src={srcImg} loading="lazy" decoding="async" />
+			<img alt={title} src={srcImg} loading="lazy" decoding="async" width={"100%"}/>
 			{title ? (
 				<Typography
 					text={
